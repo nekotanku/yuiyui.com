dc92788a2e2f558d73a485f71656a036e7ce200a
EC2 Default User <ec2-user@ip-172-31-46-167.us-east-2.compute.internal>
2019-04-17 16:17:50 +0000

    topページからのリンクのためのルーター作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get '/'=>"home#top"
   get "about"=>"home#about"
+  get "posts/index"=>"posts#index"
+  get "posts/new"=>"posts#new"
+  get "users/index"=>"users#index"
+  get "users/signup"=>"users#new"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
