def778904867ce61094e00277f80c5cc65020250
Takahiro.W <progeneral.eva8@gmail.com>
2019-04-17 15:35:26 +0000

    :memo: userモデルにバリデーションを追加
    name, emailに複数のバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
+  
+  validates :name, presence: true,
+                   length: { in: 3..20 }, 
+                   uniqueness: { case_sensitive: false }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
 end
