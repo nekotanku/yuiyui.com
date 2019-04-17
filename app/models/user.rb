fbccb3840cd1e6c3c6eef20e8e22bce6cad5d8b9
Takahiro.W <progeneral.eva8@gmail.com>
2019-04-17 16:43:13 +0000

    サインアップページ等の作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email.downcase! }
   has_secure_password
   
   validates :name, presence: true,
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
+  get 'signup', to: 'users#new'
+  
 end
