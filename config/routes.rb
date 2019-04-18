324132dd36a2edeb7aba6dd7cea74724f18f5a7a
Takahiro.W <progeneral.eva8@gmail.com>
2019-04-18 08:34:17 +0000

    :sparkles: ログイン機能の追加
    sessionコントローラを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   get '/'=>"home#top"
   get "about"=>"home#about"
   get "posts/index"=>"posts#index"
   get "posts/new"=>"posts#new"
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   get "users"=>"users#index"
   get "users/signup"=>"users#new"
-
   resources :users
 end
