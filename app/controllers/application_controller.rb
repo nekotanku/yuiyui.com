7b9cd6eb89ef36515ed11c97fe2fa4cc8077deb1
EC2 Default User <ec2-user@ip-172-31-46-167.us-east-2.compute.internal>
2019-04-20 10:14:51 +0000

    ユーザーと投稿の紐付け

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   include SessionsHelper
-  
+  def set_current_user
+    @current_user=User.find_by(id: session[:user_id])
+  end
   private
   
   def require_user_logged_in
