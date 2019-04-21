c03c1b7b6f0a76e887248202f43c0bd81c1a8115
Takahiro.W <progeneral.eva8@gmail.com>
2019-04-21 09:26:54 +0000

    :bug: 記事詳細ページのユーザリンクの修正
    ユーザリンクがpost_idを参照していたため、記事を所有するユーザのリンクが正しく指定されていなかった。

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,6 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
-  get "users"=>"users#index"
   resources :users
   resources :posts
 end
