db6476661ea38b0e9abb770707083eaa72d4ef6e
Takahiro.W <progeneral.eva8@gmail.com>
2019-04-22 11:27:48 +0000

    :+1: ルーティングの統一（再）'#11'
    postのルーティングが複数あったため統一した。

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,7 @@ Rails.application.routes.draw do
   get '/'=>"home#top"
   get "about"=>"home#about"
   
-  get "posts/index"=>"posts#index"
-  get "posts/new"=>"posts#new"
-  post "posts/create"=>"posts#create"
-  get "posts/:id"=>"posts#show"
-  get "posts/:id/edit"=>"posts#edit"
   post "posts/:id/update"=>"posts#update"
-  post "posts/:id/destroy"=>"posts#destroy"
   
   get "users/signup"=>"users#new"
   get 'login', to: 'sessions#new'
