eea62239be5a997200de52444f2171976280fc60
Takahiro.W <progeneral.eva8@gmail.com>
2019-04-23 08:01:46 +0000

    :sparkles: ユーザ編集機能追加
    ユーザ詳細ページから編集できる

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,9 +24,17 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to user_path
+    else
+      render 'edit'
+    end
+      
   end
 
   def destroy
