6e17fb9bce490960d7dacc8dc48ceb863aba43ff
EC2 Default User <ec2-user@ip-172-31-46-167.us-east-2.compute.internal>
2019-04-26 08:46:04 +0000

    ユーザー画像設定と編集

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190424102731) do
+ActiveRecord::Schema.define(version: 20190426084021) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
