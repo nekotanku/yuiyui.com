859f34fb3a0fba803d33a62c8567fc3a8c7bf291
Takahiro.W <progeneral.eva8@gmail.com>
2019-04-29 15:58:25 +0000

    :white_check_mark: userモデルのスペックを追加
    バリデーション、検索機能、follow/unfollowメソッドのテストを記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,14 @@
 FactoryBot.define do
-  factory :user do
-    name { 'yui' }
-    email { 'yui@email.com' }
+  factory :yuri, class: User do
+    name { 'yuri' }
+    email { 'yuri@email.com' }
+    password { 'password' }
+    password_confirmation { 'password' }
+  end
+  
+  factory :taro, class: User do
+    name { 'taro' }
+    email { 'taro@email.com' }
     password { 'password' }
     password_confirmation { 'password' }
   end
