1a2c4c24d0d33272434ad3fdfbe0183ce060d8a7
Takahiro.W <progeneral.eva8@gmail.com>
2019-04-29 17:58:51 +0000

    :bug: userモデルの修正
    書くべきコードが書いてなかった。これのせいでテストが通らなかった。

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,14 @@
 class Post < ApplicationRecord
-  validates :content, {presence: true, length: {maximum: 140}}
+  validates :content, {presence: true, length: {maximum: 200}}
   validates :user_id,{presence: true}
   mount_uploaders :picture, PictureUploader
   serialize :picture, JSON
-
+  
+  belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :like_users, through: :likes, source: :user
+  has_many :comments, dependent: :delete_all
+  
   def self.search(search)
     if search
       where(['content LIKE ?', "%#{search}%"])
@@ -17,7 +20,7 @@ class Post < ApplicationRecord
   def user
     return User.find_by(id: self.user_id)
   end
-  has_many :comments, dependent: :delete_all
+  
   def like(user)
     likes.create(user_id: user.id)
   end
