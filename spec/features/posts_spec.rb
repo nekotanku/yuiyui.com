a03d3e2279b04fe3e713ca3ebb18ef2095222ef3
Takahiro.W <progeneral.eva8@gmail.com>
2019-05-01 18:17:15 +0000

    :white_check_mark: Postのフィーチャスペックの追加
    記事の作成、編集、削除、検索のスペックを追加

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -1,5 +1,64 @@
 require 'rails_helper'
 
 RSpec.feature "Posts", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  given(:user) { FactoryBot.create(:yuri) }
+  given(:other_user) { FactoryBot.create(:taro) }
+  given!(:post) { FactoryBot.create(:yuri_post, user: user) }
+  given!(:other_post) { FactoryBot.create(:taro_post, user: other_user) }
+  
+  scenario '記事を投稿できること' do
+    login(user)
+    click_link "新規投稿"
+    expect {
+      fill_in "post[content]", with: "ハロー世界"
+      click_on "Post"
+    }.to change(Post, :count).by(1)
+    expect(page).to have_text "ハロー世界"
+  end
+  
+  feature '記事を編集する場合' do
+    scenario 'Post製作者のみ編集できること' do
+      login(user)
+      visit posts_path
+      click_link post.content
+      click_link "編集"
+      fill_in "post[content]", with: "ハロー編集"
+      click_on "Post"
+      expect(page).to have_text "ハロー編集"
+    end
+    
+    scenario 'Post製作者以外は編集できないこと' do
+      login(other_user)
+      visit posts_path
+      click_link post.content
+      expect(page).to have_no_link "編集"
+    end
+  end
+  
+  scenario '記事を削除できること' do
+    login(user)
+    visit posts_path
+    click_link post.content
+    expect {
+      click_link "削除"
+    }.to change(Post, :count).by(-1)
+    expect(page).to have_current_path "/posts"
+  end
+  
+  scenario '記事を検索できること(content検索)' do
+    visit posts_path
+    fill_in 'search', with: post.content
+    click_on "Search"
+    expect(page).to have_content post.content
+    expect(page).to have_no_content other_post.content
+  end
+
+  
+  def login(user)
+    visit login_path
+    click_link "ログイン"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+  end
 end
