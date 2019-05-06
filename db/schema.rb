0f698a2a59a54214762c2f264f117610ba13f82f
Takahiro.W <progeneral.eva8@gmail.com>
2019-05-06 08:32:13 +0000

    bootatrap4に移行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20190505064602) do
 
   create_table "comments", force: :cascade do |t|
@@ -55,9 +54,8 @@ ActiveRecord::Schema.define(version: 20190505064602) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "introduce"
-    t.string "avater"
     t.string "remember_digest"
-
+    t.string "avater"
   end
 
 end
