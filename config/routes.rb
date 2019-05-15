c899c51067de977a5432cfc538bf9436ffcf4d39
Takahiro.W <progeneral.eva8@gmail.com>
2019-05-15 09:35:32 +0000

    :+1:ルーティングの修正
    ログイン前後でrootを変更するようにしました

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,34 @@
 Rails.application.routes.draw do
-
   get 'relationships/create'
-
   get 'relationships/destroy'
-
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
-
-  get '/'=>"home#top"
-  get "about"=>"home#about"
+  get 'about' => 'home#about'
+  get 'users/signup' => 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
-  get "users/signup"=>"users#new"
   resources :users do
     member do
       get :followings
       get :followers
     end
-    
   end
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
-  
   resources :posts 
   resources :comments, only: [:create]
-  
   resources :likes, only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
+  
+  class AuthenticatedConstraint
+    def matches?(request)
+      request.session['user_id'].present?
+    end
+  end
+  
+  constraints AuthenticatedConstraint.new do
+    root to: 'posts#index', as: :user_root
+  end
+  root to: 'home#top'
 end
