da4b1cf35e221254c122b395eaad51b037025baf
渡辺崇博 <general.eva10@gmail.com>
2019-06-22 00:44:37 +0900

    specを修正
    全てのspecを通過させる

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -5,20 +5,20 @@ RSpec.feature "Posts", type: :feature do
   given(:other_user) { FactoryBot.create(:taro) }
   given!(:post) { FactoryBot.create(:yuri_post, user: user) }
   given!(:other_post) { FactoryBot.create(:taro_post, user: other_user) }
-  
+
   feature '記事を投稿する場合' do
     scenario '文字のみの記事を投稿できること' do
       login(user)
       visit new_post_path
       expect {
         fill_in "post[content]", with: "ハロー世界"
-        click_on "Post"
+        click_on "投稿"
       }.to change(Post, :count).by(1)
       expect(page).to have_text "ハロー世界"
     end
-    
+
   end
-  
+
   feature '記事を編集する場合' do
     scenario 'Post製作者のみ編集できること' do
       login(user)
@@ -26,10 +26,10 @@ RSpec.feature "Posts", type: :feature do
       click_link post.content
       click_link "編集"
       fill_in "post[content]", with: "ハロー編集"
-      click_on "Post"
+      click_on "更新する"
       expect(page).to have_text "ハロー編集"
     end
-    
+
     scenario 'Post製作者以外は編集できないこと' do
       login(other_user)
       visit posts_path
@@ -37,7 +37,7 @@ RSpec.feature "Posts", type: :feature do
       expect(page).to have_no_link "編集"
     end
   end
-  
+
   scenario '記事を削除できること' do
     login(user)
     visit posts_path
@@ -47,7 +47,7 @@ RSpec.feature "Posts", type: :feature do
     }.to change(Post, :count).by(-1)
     expect(page).to have_current_path "/posts"
   end
-  
+
   scenario '記事を検索できること(content検索)' do
     visit posts_path
     fill_in 'search', with: post.content
@@ -56,7 +56,7 @@ RSpec.feature "Posts", type: :feature do
     expect(page).to have_no_content other_post.content
   end
 
-  
+
   def login(user)
     visit login_path
     click_link "ログイン"
--- spec/features/sessions_spec.rb
+++ spec/features/sessions_spec.rb
@@ -2,20 +2,20 @@ require 'rails_helper'
 
 RSpec.feature "Sessions", type: :feature do
   given(:user) { FactoryBot.create(:yuri) }
-  
+
   scenario 'ログイン出来ること' do
     visit "/"
     login
-    expect(page).to have_content 'ユーザ編集'
+    expect(page).to have_current_path '/posts'
   end
-  
+
   scenario 'ログアウト出来ること' do
     visit "/"
     login
     click_on 'ログアウト'
     expect(page).to have_current_path '/'
   end
-  
+
   def login
     click_link 'ログイン'
     fill_in 'メールアドレス', with: user.email
